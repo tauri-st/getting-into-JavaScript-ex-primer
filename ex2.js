4b7d6c19e97b6a2fa804a746f2529df297cceee3
Tauri StClaire <104536950+tauri-st@users.noreply.github.com>
2023-08-17 19:56:37 +0000

    Declared variables ex2

--- ex2.js
+++ ex2.js
@@ -1,6 +1,9 @@
 //Declare a variable for money in your bank account
+var bank_account = 512.82;
 
 //Declare variables for phone and accessory costs
+const PHONE_PRICE = 99.99;
+const ACCESSORY_PRICE = 9.99;
 
 /* Function to subtract money per phone purchase in a loop 
 until bank account money runs out */
