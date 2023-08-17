dcdde1b55f6c6526641d7e1552498444e8f76df3
Tauri StClaire <104536950+tauri-st@users.noreply.github.com>
2023-08-17 19:35:02 +0000

    function and call to print favorite books

--- ex.js
+++ ex.js
@@ -6,6 +6,12 @@ function addFavoriteBooks(bookName) {
 }
 
 // TODO: define printFavoriteBooks() function
+function printFavoriteBooks() {
+	console.log(`Favorite Books: ${favoriteBooks.length}`);
+	for (let bookName of favoriteBooks) {
+		console.log(bookName);
+	}
+}
 
 var favoriteBooks = [];
 
@@ -16,3 +22,4 @@ addFavoriteBook("Great Expectations");
 addFavoriteBook("You Don't Know JS");
 
 // TODO: print out favorite books
+printFavoriteBooks();
