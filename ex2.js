97278fc2c618992e65830edbb6e3ecdc8e62d348
Tauri StClaire <104536950+tauri-st@users.noreply.github.com>
2023-08-18 00:27:41 +0000

    Attempt conditional and a while do loop to buy until money runs out

--- ex2.js
+++ ex2.js
@@ -7,5 +7,17 @@ const ACCESSORY_PRICE = 9.99;
 
 /* Function to subtract money per phone purchase in a loop 
 until bank account money runs out */
+if (PHONE_PRICE <= bank_account) {
+  bank_account -= PHONE_PRICE;
+}
+console.log(bank_account);
+do {
+  bank_account -= ACCESSORY_PRICE;
+} while (bank_account >= ACCESSORY_PRICE);
+console.log(bank_account);
 
 //Total Price of Phone Purchase
+
+/* for (i = 0; purchase < bank_account; i++) {
+    purchase = bank_account + ACCESSORY_PRICE;
+    console.log(purchase); */
