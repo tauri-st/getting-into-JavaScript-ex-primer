afdd35d15d6fd42deeaf95cd87aa5c26818a0b60
Tauri StClaire <104536950+tauri-st@users.noreply.github.com>
2023-08-21 20:35:55 +0000

    Declared purchase variable as well and do while loop to buy phones until bank account runs out

--- ex2.js
+++ ex2.js
@@ -1,19 +1,20 @@
 //Declare a variable for money in your bank account
 var bank_account = 512.82;
 
-//Declare variables for phone and accessory costs
+//Declare variables for phone, accessory costs, and tax rate and spend limit
 const PHONE_PRICE = 99.99;
 const ACCESSORY_PRICE = 9.99;
+const TAX = 0.08;
+const SPENDING_LIMIT = 200;
+
+//Declare a variable to hold final purchase amount
+var purchase = 0;
 
 /* Function to subtract money per phone purchase in a loop 
 until bank account money runs out */
-/*if (PHONE_PRICE <= bank_account) {
-  bank_account -= PHONE_PRICE;
-}
-console.log(bank_account); */
 do {
-  bank_account -= PHONE_PRICE + ACCESSORY_PRICE;
-} while (bank_account >= PHONE_PRICE + ACCESSORY_PRICE);
+  bank_account -= PHONE_PRICE;
+} while (bank_account >= PHONE_PRICE);
 console.log(bank_account);
 
 //Total Price of Phone Purchase
